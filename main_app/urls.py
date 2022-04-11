b881854fd737296babf53693c0a583a015d18198
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-04-10 23:34:23 -0400

    update job_detail name and imports

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
   path('', views.get_jobs, name='jobs'),
-  path('job/<str:pk>/', views.jobDetails, name='job-details'),
+  path('job/<str:pk>/', views.job_details, name='job-details'),
   path('create-job/', views.create_job, name="create-job"),
   path('update-job/<str:pk>', views.update_job, name='update-job'),
   path('delete-job/<str:pk>', views.delete_job, name='delete-job'),
