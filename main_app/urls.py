5312b8a54768f11dd3518991ffd8fcbfbcd66595
GravleyCM <GravleyCM@gmail.com>
2022-04-10 22:43:56 -0400

    create path for edit and delete jobs

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
   path('', views.get_jobs, name='jobs'),
   path('job/<str:pk>/', views.jobDetails, name='job-details'),
-  path('create-job/', views.create_job, name="create-job")
+  path('create-job/', views.create_job, name="create-job"),
+  path('update-job', views.update_job, name='update-job'),
+  path('delete-job', views.delete_job, name='delete-job'),
 ]
