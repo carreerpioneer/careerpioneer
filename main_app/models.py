109b38843bbb002fac3713f3ffd4a2a551edabb2
Hanibee <cyborgofthefuture@gmail.com>
2022-04-11 10:55:45 -0400

    jobdetails model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid 
+
 class Job(models.Model):
   title = models.CharField(max_length=200)
   pay_range = models.CharField(max_length=200, null=True, blank=True)
@@ -7,11 +8,18 @@ class Job(models.Model):
   # platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
   notes = models.TextField(null=True, blank=True)
   # owner = models.
-  # job_details = models.
   # status = 
   resume_groomed = models.BooleanField(null=True, blank=True)
   created = models.DateTimeField(auto_now_add=True)
   id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
+  def __str__(self):
+    return self.title
+
+class JobDetails(models.Model):
+  location = models.CharField(max_length=200)
+  remote = models.BooleanField()
+  job = models.ForeignKey(Job, on_delete=models.CASCADE)
+  
   def __str__(self):
     return self.title
