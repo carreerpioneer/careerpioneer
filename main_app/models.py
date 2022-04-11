887f42372f0a6b85d5165b3e6971cc9f561ded39
GravleyCM <GravleyCM@gmail.com>
2022-04-11 10:55:35 -0400

    add Platform model and register

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 import uuid 
+
 class Job(models.Model):
   title = models.CharField(max_length=200)
   pay_range = models.CharField(max_length=200, null=True, blank=True)
   company = models.CharField(max_length=200)
-  # platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
   notes = models.TextField(null=True, blank=True)
   # owner = models.
   # job_details = models.
@@ -15,3 +15,13 @@ class Job(models.Model):
 
   def __str__(self):
     return self.title
+
+class Platform(models.Model):
+  name = models.CharField(max_length=100)
+  website = models.CharField(max_length=100)
+  job = models.ForeignKey(Job, on_delete=models.CASCADE)
+  created = models.DateTimeField(auto_now_add=True)
+  id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+  def __str__(self):
+      return self.name
