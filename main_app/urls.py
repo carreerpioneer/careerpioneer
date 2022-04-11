3a6d9859c2d231a93383e87d58c186d016b50a9a
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-04-10 21:57:13 -0400

    working index detail view

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.jobs, name='jobs'),
-  path('job/<str:pk>', views.jobDetails, name='job-details'),
+  path('', views.get_jobs, name='jobs'),
+  path('job/<str:pk>/', views.jobDetails, name='job-details'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,30 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Job
 
-job_list = [
-  {
-    "id": 1,
-    "title": "Super Senior Dev",
-    "status": "Applied",
-  },
-  {
-    "id": 2,
-    "title": "Super Junior Dev",
-    "status": "Applied",
-  },
-  {
-    "id": 3,
-    "title": "Super Senior Dev",
-    "status": "Not Applied",
-  },
-]
-
-def jobs(request):
-  context = {'jobs': job_list}
-  print(context)
-  return render(request, "jobs/jobs.html", context)
+def get_jobs(request):
+  jobs = Job.objects.all()
+  context = {'jobs': jobs}
+  return render(request, 'jobs/jobs.html', context)
 
 def jobDetails(request, pk):
-  print(pk)
-  return HttpResponse("<h1>Job Details Page + ' ' + {{pk}}</h1>")
+  job_obj = Job.objects.get(id=pk)
+  context = {'job': job_obj}
+  return render(request, 'jobs/job-detail.html', context)
 
