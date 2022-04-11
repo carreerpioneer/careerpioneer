9ba1fb5802f3d4d6453cb4aeb96d7c24ab5162b1
GravleyCM <GravleyCM@gmail.com>
2022-04-11 15:29:49 -0400

    add path to create-status

--- main_app/models.py
+++ main_app/models.py
@@ -25,6 +25,7 @@ class Status(models.Model):
 
   def __str__(self):
     return self.name
+
 class Job(models.Model):
   title = models.CharField(max_length=200)
   pay_range = models.CharField(max_length=200, null=True, blank=True)
@@ -37,6 +38,7 @@ class Job(models.Model):
   created = models.DateTimeField(auto_now_add=True)
   id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
+  
   def __str__(self):
     return self.title
 
