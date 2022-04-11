c93411791beb7186031f6502a7333c2442f25af0
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-04-10 21:29:38 -0400

    job model complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-
-# Create your models here.
-
+import uuid 
 class Job(models.Model):
   title = models.CharField(max_length=200)
   pay_range = models.CharField(max_length=200, null=True, blank=True)
   company = models.CharField(max_length=200)
   # platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
   notes = models.TextField(null=True, blank=True)
-  owner = models.
+  # owner = models.
+  # job_details = models.
+  # status = 
+  resume_groomed = models.BooleanField(null=True, blank=True)
+  created = models.DateTimeField(auto_now_add=True)
+  id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
