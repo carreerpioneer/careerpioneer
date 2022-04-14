3e2a0c3e81c87d856948efe77fa473a949ba83ac
GravleyCM <GravleyCM@gmail.com>
2022-04-14 09:48:17 -0400

    update all models

--- main_app/models.py
+++ main_app/models.py
@@ -1,27 +1,31 @@
 from django.db import models
-import uuid 
 from django.contrib.auth.models import User
+import uuid 
 
 class JobDetail(models.Model):
   location = models.CharField(max_length=200)
   remote = models.BooleanField()
+  created = models.DateTimeField(auto_now_add=True)
   id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.location
 
 class Platform(models.Model):
   name = models.CharField(max_length=100)
-  website = models.CharField(max_length=100)
   created = models.DateTimeField(auto_now_add=True)
   id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
 
 class Status(models.Model):
   name = models.CharField(max_length=250)
+  created = models.DateTimeField(auto_now_add=True)
   id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
@@ -34,7 +38,8 @@ class Job(models.Model):
   job_details = models.ForeignKey(JobDetail, on_delete=models.CASCADE, default='')
   platform = models.ForeignKey(Platform, on_delete=models.CASCADE, default='')
   status = models.ForeignKey(Status, on_delete=models.CASCADE, default='')
-  resume_groomed = models.BooleanField(null=True, blank=True)
+  resume_groomed = models.BooleanField()
+  url = models.CharField(max_length=500, null=True, blank=True)
   created = models.DateTimeField(auto_now_add=True)
   id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
