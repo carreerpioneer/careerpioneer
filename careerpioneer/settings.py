ea0485b62e5e6b924d54bd63a753b8963956f80d
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-04-15 14:34:25 -0400

    heroku commit

--- careerpioneer/settings.py
+++ careerpioneer/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'string'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG') == 'True'
+DEBUG = 'True'
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = []
