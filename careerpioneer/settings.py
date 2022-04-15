4898b01aa17c99b3b38ae57621dcb50a88711843
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-04-15 14:06:12 -0400

    heroku push

--- careerpioneer/settings.py
+++ careerpioneer/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 from pathlib import Path
 
